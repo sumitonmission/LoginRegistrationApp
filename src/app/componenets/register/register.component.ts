6bb891a1952e113ac13f67016bb93009fe678fd6
Sumit Singh <sumitonmission@gmail.com>
2023-11-05 00:17:13 +0530

    New feature Validation added to each field

--- src/app/componenets/register/register.component.ts
+++ src/app/componenets/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,16 +13,29 @@ export class RegisterComponent implements OnInit {
   }
 
   registerform = new FormGroup({
-    firstname: new FormControl(""),
-    lastname: new FormControl(""),
-    email: new FormControl(""),
-    mobilenumber: new FormControl(""),
-    gender: new FormControl(""),
-    password: new FormControl(""),
+    firstname: new FormControl("", [Validators.required]),
+    lastname: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.email, Validators.required]),
+    mobilenumber: new FormControl("", [Validators.pattern("[1-9]{1}[0-9]{9}")]),
+    gender: new FormControl("", [Validators.required]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(12),
+    ]),
     cnfpassword: new FormControl("")
   });
 
   registeredSubmited() {
     console.log(this.registerform);
   }
+
+  get firstname(): FormControl { return this.registerform.get('firstname') as FormControl; }
+  get lastname(): FormControl { return this.registerform.get('lastname') as FormControl; }
+  get email(): FormControl { return this.registerform.get('email') as FormControl; }
+  get mobilenumber(): FormControl { return this.registerform.get('mobilenumber') as FormControl; }
+  get gender(): FormControl { return this.registerform.get('gender') as FormControl; }
+  get password(): FormControl { return this.registerform.get('password') as FormControl; }
+  get cnfpassword(): FormControl { return this.registerform.get('cnfpassword') as FormControl; }
+
 }
